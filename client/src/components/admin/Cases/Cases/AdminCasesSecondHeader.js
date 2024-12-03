0bbc11bb9bca7a3da9f690f55e655dfa0c9e6bf2
ArtZymeri <132089036+ArtZymeri@users.noreply.github.com>
2024-12-03 23:49:34 +0100

    Little tweak in CasesHeader

--- client/src/components/admin/Cases/Cases/AdminCasesSecondHeader.js
+++ client/src/components/admin/Cases/Cases/AdminCasesSecondHeader.js
@@ -1,39 +1,60 @@
-import {Button} from "@mui/material";
-import {observer} from "mobx-react-lite";
-import {DatePicker, LocalizationProvider} from "@mui/x-date-pickers";
-import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import { Button } from "@mui/material";
+import { observer } from "mobx-react-lite";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-const AdminSecondHeader = ({presenter}) => {
-
-    return (
-        <div className="admin-cases-header-container">
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker sx={{width: '100%'}} value={presenter.vm?.firstDateFilter} slotProps={{
-                    field: {
-                        clearable: true, onClear: () => {
-                            presenter.handleDatesChange(null, 'firstDateFilter')
-                        }
-                    },
-                }} label="Data fillestare" format={'DD/MM/YYYY'} onChange={(newValue) => {
-                    presenter.handleDatesChange(newValue, 'firstDateFilter')
-                }}/>
-            </LocalizationProvider>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker sx={{width: '100%'}} value={presenter.vm?.lastDateFilter} slotProps={{
-                    field: {
-                        clearable: true, onClear: () => {
-                            presenter.handleDatesChange(null, 'lastDateFilter')
-                        }
-                    },
-                }} label="Data mbyllëse" format={'DD/MM/YYYY'} onChange={(newValue) => {
-                    presenter.handleDatesChange(newValue, 'lastDateFilter')
-                }}/>
-            </LocalizationProvider>
-            <Button variant="contained" size="large" color="success" disabled={presenter?.printInvoicesButtonDisabled} sx={{textWrap: 'nowrap', flexShrink: '0'}}>
-                Printo Faturat
-            </Button>
-        </div>
-    )
-}
+const AdminSecondHeader = ({ presenter }) => {
+  return (
+    <div className="admin-cases-header-container">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          sx={{ width: "100%" }}
+          value={presenter.vm?.firstDateFilter}
+          slotProps={{
+            field: {
+              clearable: true,
+              onClear: () => {
+                presenter.handleDatesChange(null, "firstDateFilter");
+              },
+            },
+          }}
+          label="Starting Date"
+          format={"DD/MM/YYYY"}
+          onChange={(newValue) => {
+            presenter.handleDatesChange(newValue, "firstDateFilter");
+          }}
+        />
+      </LocalizationProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          sx={{ width: "100%" }}
+          value={presenter.vm?.lastDateFilter}
+          slotProps={{
+            field: {
+              clearable: true,
+              onClear: () => {
+                presenter.handleDatesChange(null, "lastDateFilter");
+              },
+            },
+          }}
+          label="Ending Date"
+          format={"DD/MM/YYYY"}
+          onChange={(newValue) => {
+            presenter.handleDatesChange(newValue, "lastDateFilter");
+          }}
+        />
+      </LocalizationProvider>
+      <Button
+        variant="contained"
+        size="large"
+        color="success"
+        disabled={presenter?.printInvoicesButtonDisabled}
+        sx={{ textWrap: "nowrap", flexShrink: "0", height: "56px" }}
+      >
+        Produce Invoices
+      </Button>
+    </div>
+  );
+};
 
 export default observer(AdminSecondHeader);
