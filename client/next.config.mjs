3d00fd24ead081d014b1b9d7cfb29c1d4d791f1d
ArtZymeri <132089036+ArtZymeri@users.noreply.github.com>
2025-01-16 23:49:10 +0100

    Deployment fix on client

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(js|mjs|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+      },
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
